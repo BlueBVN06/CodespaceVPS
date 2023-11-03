be86329ec2e39f5f5bd29d0ac60b96f7a8ae9777
BlueBVN1716 <60213759+BlueBVN06@users.noreply.github.com>
2023-11-03 14:40:46 +0700

    Update codespaces.cmd

--- codespaces.cmd
+++ codespaces.cmd
@@ -1,5 +1,6 @@
 wget -O bios64.bin "https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin"
 wget -O win10.iso "https://pixeldrain.com/u/9Bq1Z2NF"
+wget -O win10.iso "https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US"
 wget -O ngrok.tgz "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz"
 tar -xf ngrok.tgz
 rm -rf ngrok.tgz
@@ -9,5 +10,6 @@ sudo apt update
 sudo apt install qemu-kvm -y
 qemu-img create -f raw win10.img
 qemu-img create -f raw win10.img 32G
-sudo qemu-system-x86_64 -m 8G -cpu host -boot order=c -drive file=win10.iso,media=cdrom -drive file=win10.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
+sudo qemu-system-x86_64 -m 11G -cpu host -boot order=c -drive file=win10.iso,media=cdrom -drive file=win10.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -enable-kvm -accel kvm -bios bios64.bin
 sudo apt-get update && sudo apt-get upgrade
+
