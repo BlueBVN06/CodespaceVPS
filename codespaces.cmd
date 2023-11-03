59fc97d2362c26774756d13eaa8ede6922b63ab5
BlueBVN1716 <60213759+BlueBVN06@users.noreply.github.com>
2023-11-03 13:06:21 +0700

    Update codespaces.cmd

--- codespaces.cmd
+++ codespaces.cmd
@@ -9,4 +9,5 @@ sudo apt update
 sudo apt install qemu-kvm -y
 qemu-img create -f raw win10.img
 qemu-img create -f raw win10.img 32G
-sudo qemu-system-x86_64 -m 12G -cpu host -boot order=c -drive file=win10.iso,media=cdrom -drive file=win10.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
+sudo qemu-system-x86_64 -m 8G -cpu host -boot order=c -drive file=win10.iso,media=cdrom -drive file=win10.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
+sudo apt-get update && sudo apt-get upgrade
